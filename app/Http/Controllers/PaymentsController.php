f95b4c4232333da6f80751c2c6854a8476f94695
Fahmisi23 <fahm23021si@student.nurulfikri.ac.id>
2025-10-12 21:29:35 +0700

    Tugas 6 - MengUpdate dan MenDelete Data Payments

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\BaseResource;
 use App\Models\Payments;
+use App\Models\Products;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -22,8 +23,8 @@ class PaymentsController extends Controller
             'payments.tanggal_bayar',
             'payments.status',
         )
-        ->join('payment_methods', 'payment_methods.id', '=', 'payments.payment_method_id')
-        ->get();
+            ->join('payment_methods', 'payment_methods.id', '=', 'payments.payment_method_id')
+            ->get();
 
         return new BaseResource(true, 'List Data payments', $payments);
     }
@@ -75,8 +76,8 @@ class PaymentsController extends Controller
             'payments.tanggal_bayar',
             'payments.status',
         )
-        ->join('payment_methods', 'payment_methods.id', '=', 'payments.payment_method_id')
-        ->get();
+            ->join('payment_methods', 'payment_methods.id', '=', 'payments.payment_method_id')
+            ->get();
 
         return new BaseResource(true, 'List Data payments', $payments);
     }
@@ -94,7 +95,31 @@ class PaymentsController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'order_id' => 'required',
+            'payment_method_id' => 'required',
+            'jumlah_bayar' => 'required',
+            'tanggal_bayar' => 'required',
+            'status' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $payments = Payments::findOrFail($id);
+
+        if (!$payments) {
+            return new BaseResource(false, 'Produk tidak ditemukan', null);
+        }
+
+        $payments->update([
+            'order_id' => $request->order_id,
+            'payment_method_id' => $request->payment_method_id,
+            'jumlah_bayar' => $request->jumlah_bayar,
+            'tanggal_bayar' => $request->tanggal_bayar,
+            'status' => $request->status,
+        ]);
+        return new BaseResource(true, 'Data Berhasil diubah', $payments);
     }
 
     /**
@@ -102,6 +127,9 @@ class PaymentsController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $payments = Payments::find($id);
+        $payments->delete();
+
+        return new BaseResource(true, 'Data Payments Berhasil dihapus', $payments);
     }
 }
