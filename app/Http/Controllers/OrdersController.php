04e2223ebca02626df8d0d77d29ea4cced628000
fatiya17 <fatiyalabibah17@gmail.com>
2025-10-13 09:55:22 +0700

    Tugas 5 dan 6 - Table Orders

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\BaseResource;
 use Illuminate\Http\Request;
 use App\Models\Orders;
+use Illuminate\Support\Facades\Validator;
 
 class OrdersController extends Controller
 {
@@ -19,9 +20,9 @@ class OrdersController extends Controller
             'orders.total',
             'orders.status',
         )
-        ->join('customers', 'customers.id', '=', 'orders.customer_id')
-        ->get();
-            
+            ->join('customers', 'customers.id', '=', 'orders.customer_id')
+            ->get();
+
         return new BaseResource(true, 'List Data Orders', $orders);
     }
 
@@ -38,13 +39,29 @@ class OrdersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'customer_id' => 'required|numeric|min:0|exists:customers,id',
+            'tanggal_pesan' => 'required|date',
+            'total' => 'required|numeric|min:0',
+            'status' => 'required|in:pending,dikirim,dibayar,selesai,batal',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        $orders = Orders::create([
+            'customer_id' => $request->customer_id,
+            'tanggal_pesan' => $request->tanggal_pesan,
+            'total' => $request->total,
+            'status' => $request->status,
+        ]);
+
+        return new BaseResource(true, 'Berhasil Menambahkan Data Orders', $orders);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id) 
+    public function show(string $id)
     {
         $orders = Orders::select(
             'customers.name as nama_customer',
@@ -52,10 +69,10 @@ class OrdersController extends Controller
             'orders.total',
             'orders.status',
         )
-        ->join('customers', 'customers.id', '=', 'orders.customer_id')
-        ->where('orders.id', '=', $id)
-        ->get();
-        
+            ->join('customers', 'customers.id', '=', 'orders.customer_id')
+            ->where('orders.id', '=', $id)
+            ->get();
+
         return new BaseResource(true, 'Detail data orders', $orders);
     }
 
@@ -72,7 +89,31 @@ class OrdersController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'customer_id' => 'required|numeric|min:0|exists:customers,id',
+            'tanggal_pesan' => 'required|date',
+            'total' => 'required|numeric|min:0',
+            'status' => 'required|in:pending,dikirim,dibayar,selesai,batal',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $orders = Orders::findOrFail($id);
+
+        if (!$orders) {
+            return new BaseResource(false, 'Data tidak ditemukan', null);
+        }
+
+        $orders->update([
+            'customer_id' => $request->customer_id,
+            'tanggal_pesan' => $request->tanggal_pesan,
+            'total' => $request->total,
+            'status' => $request->status,
+        ]);
+
+        return new BaseResource(true, 'Berhasil Menambahkan Data Orders', $orders);
     }
 
     /**
@@ -80,6 +121,11 @@ class OrdersController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $orders = Orders::find($id);
+        $orders->orderItems()->delete();
+        $orders->payment()->delete();
+        $orders->delete();
+
+        return new BaseResource(true, 'Data Orders Berhasil dihapus', $orders);
     }
 }
